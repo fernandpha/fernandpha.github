cd3f2384fd1b34b9cecdbabc8e7b0393c08d23bd
Fernando Panduro <fernandogp2003pro@gmail.com>
2022-06-08 13:58:49 -0500

    Modificado estilos del header

--- Script.js
+++ Script.js
@@ -3,9 +3,11 @@ document.querySelector(".menu__hamburguesa").addEventListener("click", animateMe
 var line1__bars = document.querySelector(".list1__bars-menu");
 var line2__bars = document.querySelector(".list2__bars-menu");
 var line3__bars = document.querySelector(".list3__bars-menu");
+var container__nav = document.querySelector(".container__nav");
 
 function animateMenu() {
     line1__bars.classList.toggle("activelist1__bars-menu");
     line2__bars.classList.toggle("activelist2__bars-menu");
     line3__bars.classList.toggle("activelist3__bars-menu");
+    container__nav.classList.toggle("activecontainer__nav");
 }
